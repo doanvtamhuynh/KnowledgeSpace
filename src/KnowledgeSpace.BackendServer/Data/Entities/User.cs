0ed01cd32917998d0b35f891b207669d412de71b
doanvtamhuynh <huynhdoan.2132@gmail.com>
2025-08-19 20:12:49 +0700

    Config ApplicationDbContext and Fluent API

--- src/KnowledgeSpace.BackendServer/Data/Entities/User.cs
+++ src/KnowledgeSpace.BackendServer/Data/Entities/User.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KnowledgeSpace.BackendServer.Data.Entities
 {
-    [Table("Users")]
     public class User : IdentityUser
     {
         [MaxLength(50)]
